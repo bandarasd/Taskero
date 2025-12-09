a59d702b6fe4e20366b07be8ba4adc9ee54cedb0
Dananjaya Bandara <hello.danjay@gmail.com>
2025-12-09 21:00:45 +0530

    Initial Commit

---
+++ Taskero/TaskeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskeroApp.swift
+//  Taskero
+//
+//  Created by Dananjaya Bandara on 2025-12-09.
+//
+
+import SwiftUI
+
+@main
+struct TaskeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskeroTests/TaskeroTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskeroTests.swift
+//  TaskeroTests
+//
+//  Created by Dananjaya Bandara on 2025-12-09.
+//
+
+import Testing
+@testable import Taskero
+
+struct TaskeroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
